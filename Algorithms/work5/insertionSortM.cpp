32d36e0cc983f382a92e9643ff1922b8e00a56f5
Kostya Lavrenov <lavrenov.konstantin17@gmail.com>
2024-11-07 16:13:11 +0300

    Исправление багов

--- Algorithms/work5/insertionSortM.cpp
+++ Algorithms/work5/insertionSortM.cpp
@@ -2,16 +2,20 @@
 
 int main() {
 	int mas[10] = { 7,1,4,2,7,3,9,23,4,5 };
-	int temp, cur_num;
+	int temp, cur_num, bin_del;
 	for (int i = 1; i < 10; i++) {
 		temp = mas[i];
+		bin_del = i / 4;
+		if (bin_del == 0) {
+			bin_del = 1;
+		}
 		cur_num = i / 2;
 		while (temp < mas[cur_num] || temp > mas[cur_num + 1]) {
 			if (temp > mas[cur_num + 1]) {
-				cur_num += cur_num / 2;
+				cur_num += bin_del;
 			}
 			else if (temp < mas[cur_num]) {
-				cur_num /= 2;
+				cur_num -= bin_del;
 			}
 			if (cur_num == 0) {
 				if (temp >= mas[cur_num]) {
@@ -21,6 +25,10 @@ int main() {
 					cur_num--;
 					break;
 				}
+			}
+			bin_del /= 2;
+			if (bin_del == 0) {
+				bin_del = 1;
 			}
 		}
 		for (int j = i; j > cur_num + 1; j--) {
